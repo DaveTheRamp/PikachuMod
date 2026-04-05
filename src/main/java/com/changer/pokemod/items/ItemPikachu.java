94ad0046922f2eb2c8bda449ebfd475431b7ae80
DaveTheRamp <dsimonyan100@gmail.com>
2026-04-05 21:27:58 +0400

    Create ItemPikachu.java

---
+++ src/main/java/com/changer/pokemod/items/ItemPikachu.java
@@ -0,0 +1,12 @@
+package com.changer.pokemod.items;
+
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+
+public class ItemPikachu extends Item {
+    public ItemPikachu() {
+        setUnlocalizedName("pikachu_item");
+        setRegistryName("pikachu_item");
+        setCreativeTab(CreativeTabs.MISC);
+    }
+}
