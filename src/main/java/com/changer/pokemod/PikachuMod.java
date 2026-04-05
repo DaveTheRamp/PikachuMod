e2a742a50bc57d6fdb93d8a8751dd4f086a8737b
DaveTheRamp <dsimonyan100@gmail.com>
2026-04-06 00:42:33 +0400

    Create PikachuMod.java

---
+++ src/main/java/com/changer/pokemod/PikachuMod.java
@@ -0,0 +1,12 @@
+package com.changer.pokemod;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "pokemod", name = "Pikachu Mod", version = "1.0")
+public class PikachuMod {
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        // This is where the mod starts!
+    }
+}
