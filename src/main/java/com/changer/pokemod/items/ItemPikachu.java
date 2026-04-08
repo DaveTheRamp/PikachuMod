f8f25493f7681033ca85a75a5bc0d4a96dead6be
DaveTheRamp <dsimonyan100@gmail.com>
2026-04-08 18:48:11 +0400

    Update ItemPikachu.java

--- src/main/java/com/changer/pokemod/items/ItemPikachu.java
+++ src/main/java/com/changer/pokemod/items/ItemPikachu.java
@@ -1,10 +1,11 @@
-package com.changer.pokemod.items; // This MUST match the folder path above
+package com.changer.pokemod.items;
 
 import net.minecraft.item.Item;
 import net.minecraft.creativetab.CreativeTabs;
 
 public class ItemPikachu extends Item {
     public ItemPikachu() {
+        // Must match your assets folder names
         setUnlocalizedName("pikachu_item");
         setRegistryName("pokemod", "pikachu_item"); 
         setCreativeTab(CreativeTabs.MISC);
