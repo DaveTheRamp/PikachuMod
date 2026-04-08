e48b88ab80acc91cdc36e57d6bdb03d07e5e9468
DaveTheRamp <dsimonyan100@gmail.com>
2026-04-08 21:59:05 +0400

    Update ItemPikachu.java

--- src/main/java/com/changer/pokemod/items/ItemPikachu.java
+++ src/main/java/com/changer/pokemod/items/ItemPikachu.java
@@ -1,13 +1,45 @@
 package com.changer.pokemod.items;
 
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
 import net.minecraft.creativetab.CreativeTabs;
 
 public class ItemPikachu extends Item {
     public ItemPikachu() {
-        // Must match your assets folder names
         setUnlocalizedName("pikachu_item");
-        setRegistryName("pokemod", "pikachu_item"); 
+        setRegistryName("pokemod", "pikachu_item");
         setCreativeTab(CreativeTabs.MISC);
+        setMaxStackSize(1);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+        ItemStack stack = player.getHeldItem(hand);
+        if (!world.isRemote) {
+            RayTraceResult lookAt = player.rayTrace(50.0, 1.0F);
+            if (lookAt != null) {
+                double x, y, z;
+                if (lookAt.typeOfHit == RayTraceResult.Type.BLOCK) {
+                    x = lookAt.getBlockPos().getX();
+                    y = lookAt.getBlockPos().getY();
+                    z = lookAt.getBlockPos().getZ();
+                } else if (lookAt.typeOfHit == RayTraceResult.Type.ENTITY) {
+                    x = lookAt.entityHit.posX;
+                    y = lookAt.entityHit.posY;
+                    z = lookAt.entityHit.posZ;
+                } else {
+                    return new ActionResult<>(EnumActionResult.PASS, stack);
+                }
+                world.addWeatherEffect(new EntityLightningBolt(world, x, y, z, false));
+            }
+        }
+        return new ActionResult<>(EnumActionResult.SUCCESS, stack);
     }
 }
