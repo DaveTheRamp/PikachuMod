0a281116fbf2fa2a1dae238ea381ae3dd2f94314
DaveTheRamp <dsimonyan100@gmail.com>
2026-04-10 12:59:03 +0400

    Update ItemPikachu.java

--- src/main/java/com/changer/pokemod/items/ItemPikachu.java
+++ src/main/java/com/changer/pokemod/items/ItemPikachu.java
@@ -13,12 +13,10 @@ import net.minecraft.creativetab.CreativeTabs;
 
 public class ItemPikachu extends Item {
     public ItemPikachu() {
-        // This MUST match the entry in your en_us.lang file exactly
-        setUnlocalizedName("pikachu_item"); 
+        // We add "pokemod." here to force the game to look in your lang file
+        setUnlocalizedName("pokemod.pikachu_item"); 
         setRegistryName("pokemod", "pikachu_item");
         setCreativeTab(CreativeTabs.MISC);
-        
-        // No durability and infinite uses as requested
         setMaxStackSize(1);
     }
 
@@ -26,21 +24,17 @@ public class ItemPikachu extends Item {
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
         ItemStack stack = player.getHeldItem(hand);
 
-        // Server-side check to ensure lightning is synced for everyone
         if (!world.isRemote) {
-            // Shoots a 'ray' 50 blocks forward to see what you are looking at
             RayTraceResult lookAt = player.rayTrace(50.0, 1.0F);
 
             if (lookAt != null) {
                 double x, y, z;
 
-                // If you hit a block, get those coordinates
                 if (lookAt.typeOfHit == RayTraceResult.Type.BLOCK) {
                     x = lookAt.getBlockPos().getX();
                     y = lookAt.getBlockPos().getY();
                     z = lookAt.getBlockPos().getZ();
                 } 
-                // If you hit a mob/entity, get their coordinates
                 else if (lookAt.typeOfHit == RayTraceResult.Type.ENTITY) {
                     x = lookAt.entityHit.posX;
                     y = lookAt.entityHit.posY;
@@ -50,7 +44,7 @@ public class ItemPikachu extends Item {
                     return new ActionResult<>(EnumActionResult.PASS, stack);
                 }
 
-                // Summon lightning at the target (false = no fire starting)
+                // Summons lightning (false = doesn't set blocks on fire)
                 world.addWeatherEffect(new EntityLightningBolt(world, x, y, z, false));
             }
         }
