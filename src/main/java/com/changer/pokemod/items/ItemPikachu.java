cb4024c12ef435b375292017e77089222c33bf5a
DaveTheRamp <dsimonyan100@gmail.com>
2026-04-10 14:06:18 +0400

    Update ItemPikachu.java

--- src/main/java/com/changer/pokemod/items/ItemPikachu.java
+++ src/main/java/com/changer/pokemod/items/ItemPikachu.java
@@ -13,9 +13,10 @@ import net.minecraft.creativetab.CreativeTabs;
 
 public class ItemPikachu extends Item {
     public ItemPikachu() {
-        // We add "pokemod." here to force the game to look in your lang file
-        setUnlocalizedName("pokemod.pikachu_item"); 
-        setRegistryName("pokemod", "pikachu_item");
+        // This is the 'Key' the game uses to look up the name
+        setUnlocalizedName("pikachu_item"); 
+        // This is the 'ID' for textures and models
+        setRegistryName("pikachu_item");
         setCreativeTab(CreativeTabs.MISC);
         setMaxStackSize(1);
     }
@@ -23,32 +24,24 @@ public class ItemPikachu extends Item {
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
         ItemStack stack = player.getHeldItem(hand);
-
         if (!world.isRemote) {
             RayTraceResult lookAt = player.rayTrace(50.0, 1.0F);
-
             if (lookAt != null) {
                 double x, y, z;
-
                 if (lookAt.typeOfHit == RayTraceResult.Type.BLOCK) {
                     x = lookAt.getBlockPos().getX();
                     y = lookAt.getBlockPos().getY();
                     z = lookAt.getBlockPos().getZ();
-                } 
-                else if (lookAt.typeOfHit == RayTraceResult.Type.ENTITY) {
+                } else if (lookAt.typeOfHit == RayTraceResult.Type.ENTITY) {
                     x = lookAt.entityHit.posX;
                     y = lookAt.entityHit.posY;
                     z = lookAt.entityHit.posZ;
-                } 
-                else {
+                } else {
                     return new ActionResult<>(EnumActionResult.PASS, stack);
                 }
-
-                // Summons lightning (false = doesn't set blocks on fire)
                 world.addWeatherEffect(new EntityLightningBolt(world, x, y, z, false));
             }
         }
-
         return new ActionResult<>(EnumActionResult.SUCCESS, stack);
     }
 }
